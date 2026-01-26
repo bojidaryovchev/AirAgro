0e104db63340b2873e8f04a32bfc29211d3aeceb
chad-warrior <junginu763@gmail.com>
2026-01-26 17:34:47 +0200

    style: Reduce header height

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar = () => {
           : "bg-transparent"
       }`}
     >
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-2">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-1">
         {/* Logo */}
         <a href="#" className="flex items-center">
           <img
